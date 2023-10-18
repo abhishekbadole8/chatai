3f5a90eab8e7bdbff5bb953ee9aaf06cfaec6a61
Abhishek Badole <abhishekbadole8@gmail.com>
2023-10-18 17:47:04 +0530

    completed UI

---
+++ src/components/Main/Main.jsx
@@ -0,0 +1,73 @@
+import "./Main.css"
+
+function Main() {
+    return (
+        <div className="Main">
+            <main>
+                <ul>
+                    <li className="engine">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape left"/>
+                    </li>
+                    <li className="user">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape right"/>
+                    </li>
+                    <li className="engine">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape left"/>
+                    </li>
+                    <li className="user">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape right"/>
+                    </li>
+                    <li className="engine">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape left"/>
+                    </li>
+                    <li className="user">
+                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
+                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
+                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
+                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
+                        </p>
+                        <p className="list-dat">21 Aug | 08:00</p>
+                        <span className="shape right"/>
+                    </li>
+                    
+                </ul>
+            </main>
+            <form>
+                <input type="text" placeholder="Enter Input...." />
+                <button type="submit">Submit</button>
+            </form>
+        </div>
+    )
+}
+
+export default Main;
