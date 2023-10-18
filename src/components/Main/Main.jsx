7f3c61041d964abe645edd28d1db364b23c2dd39
Abhishek Badole <abhishekbadole8@gmail.com>
2023-10-18 20:09:56 +0530

    api connected

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,70 +1,67 @@
+import { useEffect, useState } from "react";
 import "./Main.css"
+import axios from "axios";
 
 function Main() {
+    const [chats, setChats] = useState([])
+    const [inputValue, setInputValue] = useState("")
+    const [loading, setLoading] = useState(false)
+
+    const fetchChat = async () => {
+        try {
+            const response = await axios.post(`https://chatai-aqub.onrender.com/api/chat`, {
+                message: [
+                    {
+                        "role": "user",
+                        "content": inputValue
+                    }
+                ]
+            })
+            if (response.data) {
+                const data = response.data
+                setChats(prevChats => [
+                    ...prevChats,
+                    {
+                        role: "user",
+                        content: inputValue
+                    },
+                    { ...data }
+                ]);
+                setLoading(false)
+            }
+        } catch (error) {
+            setLoading(false)
+            console.log(`error getting message`, error);
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        if (inputValue) {
+            setLoading(true)
+            fetchChat();
+            setInputValue("")
+        }
+    }
+
     return (
         <div className="Main">
             <main>
-                <ul>
-                    <li className="engine">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape left"/>
-                    </li>
-                    <li className="user">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape right"/>
-                    </li>
-                    <li className="engine">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape left"/>
-                    </li>
-                    <li className="user">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape right"/>
-                    </li>
-                    <li className="engine">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape left"/>
-                    </li>
-                    <li className="user">
-                        <p className="list-desc"> Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nobis inventore culpa
-                            placeat eaque pariatur illum. Quisquam nemo id sunt iste dolore doloribus, assumenda
-                            exercitationem ex minima? Ad odit maiores natus minus hic voluptatem neque nam.
-                            eos itaque ipsam aliquam non ducimus officia numquam omnis, rem tempora expedita.
-                        </p>
-                        <p className="list-dat">21 Aug | 08:00</p>
-                        <span className="shape right"/>
-                    </li>
-                    
+                <ul id="chat-list">
+                    {chats?.map((chat, index) => {
+                        const { role, content } = chat
+                        return <li key={index} className={role}>
+                            <p className="list-desc">{content}</p>
+                            <p className="list-dat">21 Aug | 08:00</p>
+                            <span className={role === "user" ? "shape right" : "shape left"} />
+                        </li>
+                    })}
+
                 </ul>
             </main>
-            <form>
-                <input type="text" placeholder="Enter Input...." />
-                <button type="submit">Submit</button>
+            <form onSubmit={handleSubmit}>
+                <input type="text" placeholder="Enter Input...." value={inputValue} onChange={(e) => setInputValue(e.target.value)} />
+                <button type="submit" disabled={loading}>Submit</button>
             </form>
         </div>
     )
